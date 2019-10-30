6878b40be87cbaa4c8ee992549a81b213e80030a
Luna9804 <claudialunamarcelino@students.ptechnyc.org>
2019-10-30 22:26:17 +0000

    work on the rest of the project to change the input into pig latin

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+console.log("hi");
+
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-$()
+$("#button").click(function(){
+    let word=
+$(".input").val();
+//you would need to write the rest of the function
+$(".output").text(word);
+console.log(word);
 
-
-
-
-
+})
 
 });
